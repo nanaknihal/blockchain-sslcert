5c4805a13192c0233d14c17a267c324f2984c873
nanaknihal <nanaknihal@gmail.com>
2022-05-11 20:05:48 -0400

    changed VerifySSLCertifcate to almost work with new code

--- contracts/VerifySSLCertificate.sol
+++ contracts/VerifySSLCertificate.sol
@@ -1,44 +1,58 @@
-// //SPDX-License-Identifier: Unlicense
-// pragma solidity ^0.8.0;
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
 
-// import { WTFUtils } from "contracts/WTFUtils.sol";
-// import "hardhat/console.sol";
+import { WTFUtils } from "contracts/WTFUtils.sol";
+import "hardhat/console.sol";
 
-// contract VerifySSLCertificate {
-//     // e and n are exponent and modulus of the authority's public key
-//     bytes public e;
-//     bytes public n;
-//     // pubkeyStart is the bytes that public key part of the tbsCertifcate shoud start with
-//     bytes public pubkeyStart; 
-//     // pubkeyLength is how long the public key part of the tbsCertificate should be
-//     uint8 public pubkeyLength;
+contract VerifySSLCertificate {
+    struct OwnershipInfo {
+        bytes pubkeyModulus;
+        bytes domainName;
+    }
 
-//     // bytes public domainNameBottomBread; 
-//     // bytes public domainNameTopBread;
+    // e and n are exponent and modulus of the authority's public key
+    uint256 public e;
+    bytes public n;
+    // pubkeyStart is the bytes that public key part of the tbsCertifcate shoud start with
+    bytes public pubkeyStart; 
+    // pubkeyLength is how long the public key part of the tbsCertificate should be
+    uint8 public pubkeyLength;
 
-//     constructor(bytes calldata e_, bytes calldata n_) {
-//         e = e_;
-//         n = n_;
-//         pubkeyStart = pubkeyStart_;
-//         pubkeyLength = pubkeyLenght_;
-//     }
+    mapping(bytes => address) public domainToAddr;
+    mapping(address => bytes) public addrToDomain;
 
-//     function keyRotate(bytes calldata newE, bytes calldata newN) public onlyOwner {
-//         e = newE;
-//         n = newN;
-//     }
 
-//     function verifyMe(bytes tbsCertificate, bytes signature, WTFUtils.ProposedSandwichAt tbsDomainName, WTFUtils.ProposedSandwichAt tbsPubkey, bytes mySignedAddress) {
-//         // 1. Require that CA signed tbsCertificate
-//         require(
-//             WTFUtils.verifyRSASignature(e, n, signature, tbsCertifcate),
-//             "Validation of certifiate signature failed"
-//         );
-//         // 2. If valid signature, check for the domain name and public key listen in tbsCertificate. These both belong to the website owner
-//         require(tbsPubKey.sandwichValue.length == pubkey.length, "proposed RSA public key has wrong length");
-//         // requrie(domain name???)
-//         // 3. check that signedAddress == msg.sender and signedAddress was signed by the website owner's private key (i.e., validated against their public key)
-//         require(signedAddress == msg.sender, "signed address should be the one you submitted this transaction with");
-//         require(WTFUtils.verifyRSASignature(tbsPubKey, 65537, msg.sender, mySignedAddress), "failed to validate signature of your address");
-//     }
-// }
+
+    constructor(uint256 e_, bytes memory n_) {
+        e = e_;
+        n = n_;
+    }
+
+    // function keyRotate(bytes calldata newE, bytes calldata newN) public onlyOwner {
+    //     e = newE;
+    //     n = newN;
+    // }
+
+    // gets the owner domain name and private keyfrom the SSL certificate in DER format
+    function getCertOwner(bytes memory tbsCertificate) public view returns (OwnershipInfo memory subject) {
+
+    }
+    
+    function verifyMe(bytes memory tbsCert, bytes calldata tbsCertSignature, bytes memory addr, bytes calldata signedAddr) public {
+        // 1. Require that CA signed tbsCertificate
+        require(
+            WTFUtils.verifyRSASignature(e, n, tbsCertSignature, tbsCert),
+            "Validation of certifiate signature failed"
+        );
+        // 2. If valid signature, get for the domain name and public key listen in tbsCertificate. These both belong to the website owner
+        OwnershipInfo memory certOwner = getCertOwner(tbsCert);
+
+        // 3. Verify signedAddr is signed by certOwner
+        require(WTFUtils.verifyRSASignature(65537, certOwner.pubkeyModulus, addr, signedAddr), "failed to validate signature of your address");
+        
+        // 4. Give domain name to addr
+        address asAddr = WTFUtils.bytesToAddress(addr);
+        domainToAddr[certOwner.domainName] = asAddr;
+        addrToDomain[asAddr] = certOwner.domainName;
+    }
+}
